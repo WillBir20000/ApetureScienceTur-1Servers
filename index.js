e70341cfbe099b72bc605360cd2b870ebd616591
Joshua Murphy <willbir360@gmail.com>
2026-04-30 09:27:52 -0500

    added start script

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 // homepage
 app.get("/", (req, res) => {
     res.send("🔥 My API is running!");
